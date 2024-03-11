ca744d71a805e4f1dfd19c00734d03fd8fa18634
vitorcarnieli <vitorcarnieli135@gmail.com>
2024-03-11 10:45:08 -0300

    init

---
+++ src/main/java/com/github/vitorcarnieli/laptopmanager/LaptopManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.vitorcarnieli.laptopmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vitorcarnieli/laptopmanager/LaptopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorcarnieli.laptopmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
